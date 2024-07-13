9166a50cf77b2f4a5904b5ca38f8d98212d2d2dd
Aman Kumar Verma <116552372+18-05-Amankumarverma@users.noreply.github.com>
2024-07-13 13:34:02 +0530

    frontPage done

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, View, Image, TextInput, TouchableOpacity, SafeAreaView } from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{flex:1}}>
+      <View style={styles.mainBody}>
+        <Text style={styles.mainHeading}>What is your name?</Text>
+        <View style={styles.container}>
+          <Image
+            source={require('./assets/fitness.png')}  // Replace with your image source
+            style={styles.image}
+            resizeMode="contain"  // Adjust the resizeMode as per your requirement
+          />
+        </View>
+        <TextInput
+          style={styles.textInput}
+          multiline={true}
+          numberOfLines={4}  // Adjust as needed, or remove to allow unlimited lines
+          placeholder="Type your text here..."
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Next</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+
   );
 }
 
 const styles = StyleSheet.create({
+  mainBody: {
+    paddingTop: 130,
+    padding: 5,
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "#192026"
+  },
+  mainHeading: {
+    fontSize: 50,
+    textAlign: "center",
+    fontWeight: "bold",
+    marginBottom: 160,
+    color: "#fff",
+    fontFamily: ''
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 400,  // Adjust the width and height as per your image's aspect ratio
+    height: 400,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 4,
+    paddingLeft: 15,
+    fontSize: 16,
+    height: 50,
+    width: "85%",
+    marginTop: 170,
+    backgroundColor: "#fff"
+  },
+  button: {
+    backgroundColor: '#3498db',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+    width: "85%",
+    alignItems: 'center',
+    marginBottom: 100
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
   },
 });
